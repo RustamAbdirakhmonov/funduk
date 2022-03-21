50ad477245a81e7434bc0a09da8c2c9cca667e4a
telgram707707@gmail.com <telgram707707@gmail.com>
2022-03-21 23:12:29 +0500

    funduk

--- lib/presentation/widgets/main_drawer.dart
+++ lib/presentation/widgets/main_drawer.dart
@@ -1,9 +1,4 @@
-import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:funduk_app/bussines_logic/cubits/counter_dart_cubit.dart';
-import 'package:funduk_app/presentation/screens/cart_screen.dart';
-import 'package:funduk_app/presentation/screens/my_home_page.dart';
 
 class MainDrawer extends StatefulWidget {
   Color color;
@@ -15,9 +10,9 @@ class MainDrawer extends StatefulWidget {
 }
 
 class _DrawerState extends State<MainDrawer> {
-  int _slectedIndex = 0;
+  int _selectIndex = 0;
 
-  final Shader linearGradient = const LinearGradient(
+  final Shader linearGradient = LinearGradient(
     colors: <Color>[
       Colors.yellow,
       Colors.red,
@@ -25,167 +20,59 @@ class _DrawerState extends State<MainDrawer> {
       Colors.yellow,
       Colors.deepOrangeAccent
     ],
-  ).createShader(const Rect.fromLTWH(0.0, 0.0, 200.0, 70.0));
+  ).createShader(Rect.fromLTWH(0.0, 0.0, 200.0, 70.0));
 
   @override
   Widget build(BuildContext context) {
+
     var width = MediaQuery.of(context).size.width;
     var height = MediaQuery.of(context).size.height;
-    return BlocBuilder<CounterDartCubit, CounterState>(
-        builder: (context, state) {
-      return NavigationRail(
-        minWidth: width * .18,
-        backgroundColor: widget.color,
-        selectedLabelTextStyle:
-            const TextStyle(color: Colors.white, fontSize: 20),
-        unselectedLabelTextStyle:
-            TextStyle(color: Colors.white.withOpacity(.6), fontSize: 16),
-        labelType: NavigationRailLabelType.all,
-        selectedIconTheme: const IconThemeData(color: Colors.red, size: 28),
-        unselectedIconTheme:
-            BlocProvider.of<CounterDartCubit>(context).getNavRailIndex() == 1
-                ? IconThemeData(color: Colors.red, size: 28)
-                : IconThemeData(color: Colors.white, size: 26),
-        groupAlignment: 0,
-        extended: false,
-        destinations: [
-          NavigationRailDestination(
-            icon: const SizedBox(),
+    return NavigationRail(
+      labelType: NavigationRailLabelType.all,
+      minWidth: width * .12,
+      backgroundColor: widget.color,
+      selectedLabelTextStyle: TextStyle(color: Colors.white, fontSize: 18),
+      unselectedLabelTextStyle: TextStyle(color: Colors.white.withOpacity(.6), fontSize: 18),
+      destinations: [
+
+        NavigationRailDestination(
+            icon: SizedBox(),
             label: RotatedBox(
               quarterTurns: -1,
               child: Text(
                 'FUNDUK',
                 style: TextStyle(
                     fontWeight: FontWeight.w700,
-                    fontSize: 25,
+                    fontSize: 40,
                     foreground: Paint()..shader = linearGradient),
               ),
-            ),
-          ),
-          const NavigationRailDestination(
-            icon: Icon(
-              Icons.shopping_cart,
-              color: Colors.white,
-              size: 25,
-            ),
-            label: SizedBox(),
-          ),
-          NavigationRailDestination(
-              icon: SizedBox(),
-              label: RotatedBox(
-                quarterTurns: -1,
-                child: Text(
-                  "typeMeal".tr(),
-                  // style: TextStyle(color: Colors.white),
-                ),
-              )),
-          NavigationRailDestination(
-              icon: SizedBox(),
-              label: RotatedBox(
-                quarterTurns: -1,
-                child: Text("typeMeal1".tr()),
-              )),
-          NavigationRailDestination(
-              icon: SizedBox(),
-              label: RotatedBox(
-                quarterTurns: -1,
-                child: Text(
-                  "salad".tr(),
-                  style: TextStyle(),
-                ),
-              )),
-          NavigationRailDestination(
-              icon: SizedBox(),
-              label: RotatedBox(
-                quarterTurns: -1,
-                child: Text(
-                  "drink".tr(),
-                  style: TextStyle(),
-                ),
-              )),
-          const NavigationRailDestination(
-              icon: SizedBox(),
-              label: CircleAvatar(
-                foregroundColor: Colors.pink,
-                maxRadius: 15,
-                backgroundImage: AssetImage('assets/images/uzbekistan-flag.jpg'),
-                child: Text('uz',),
-              )),
-          const NavigationRailDestination(
-              icon: SizedBox(),
-              label: CircleAvatar(
-                foregroundColor: Colors.pink,
-                maxRadius: 15,
-                backgroundImage: AssetImage('assets/images/russian-flag.png',),
-                child: Text('ru'),
-              ))
-        ],
-        selectedIndex: state.navIndex,
-        onDestinationSelected: (int index) {
-          BlocProvider.of<CounterDartCubit>(context).setNavRailIndex(index);
+            )),
+        NavigationRailDestination(
+            icon: Icon(Icons.shopping_cart,color:Colors.white,size: 30,), label: SizedBox()),
 
-          switch (state.navIndex) {
-            case 0:
-              Navigator.of(context).pushNamed(MyHomePage.routeArgs);
-              break;
-            case 1:
-              {
-                Navigator.of(context).pushNamed(CartScreen.routeArgs);
-                setState(() {
-                  _slectedIndex = 1;
-                });
-              }
-              break;
-            case 2:
-              {
-                setState(() {
-                  _slectedIndex = 2;
-                });
-              }
-              break;
-            case 3:
-              {
-                setState(() {
-                  _slectedIndex = 3;
-                });
-              }
-              ;
-              break;
-            case 4:
-              {
-                setState(() {
-                  _slectedIndex = 4;
-                });
-              }
-              break;
-            case 5:
-              {
-                setState(() {
-                  _slectedIndex = 5;
-                });
-              }
-              break;
-            case 6:
-              {
-                context.setLocale(Locale('uz', 'UZ'));
-                BlocProvider.of<CounterDartCubit>(context)
-                    .setNavRailIndex(_slectedIndex);
-                BlocProvider.of<CounterDartCubit>(context).setLang(1);
-              }
-              break;
-            case 7:
-              {
-                context.setLocale(Locale('ru', 'RU'));
-                BlocProvider.of<CounterDartCubit>(context)
-                    .setNavRailIndex(_slectedIndex);
-                BlocProvider.of<CounterDartCubit>(context).setLang(2);
+        NavigationRailDestination(icon: SizedBox(), label: RotatedBox(
+          quarterTurns: -1,
+          child: Text('Европейская'),
+        )),
+        NavigationRailDestination(icon: SizedBox(), label: RotatedBox(
+          quarterTurns: -1,
+          child: Text('Японская'),
+        )),
+        NavigationRailDestination(icon: SizedBox(), label: RotatedBox(
+          quarterTurns: -1,
+          child: Text('Напитки'),
+        )),
+        NavigationRailDestination(icon: SizedBox(), label:CircleAvatar(child: Text('uz'),)),
+        NavigationRailDestination(icon: SizedBox(), label:CircleAvatar(child: Text('ru'),))
 
-              }
-              break;
-            //
-          }
-        },
-      );
-    });
+
+      ],
+      selectedIndex: _selectIndex,
+      onDestinationSelected: (int index) {
+        setState(() {
+          _selectIndex = index;
+        });
+      },
+    );
   }
 }
